2436dd4f1c45c60094aec7d7e05aade54f693fe7
Mark Pasricha <markp862@gmail.com>
2020-07-01 20:52:07 -0400

    First graph done

---
+++ config.py
@@ -0,0 +1,2 @@
+database = "postgres"
+password = "postgres"
--- sqlData.py
+++ sqlData.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import sessionmaker
+from config import database, password
 import pandas as pd 
+import matplotlib.pyplot as plt
+import time
+from tqdm import tqdm
 
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/company_db', echo=False)
+
+engine = create_engine(f'postgresql+psycopg2://{database}:{password}@localhost:5432/company_db', echo=False)
 
 Base = declarative_base()
 
@@ -35,14 +40,29 @@ class Salaries(Base):
 
 
 Session = sessionmaker(bind=engine)
-session = Session()
+s = Session()
 
 # result = session.query(Employees).all()
 # results2 = session.query(Salaries).all() 
 # results3 = session.query(Titles).all()
 
-results = session.query(Employees, Titles).join(Titles, Titles.title_id == Employees.emp_title_id).all()
+results = s.query(Employees, Titles, Salaries).join(Titles,Titles.title_id == Employees.emp_title_id).join(Salaries,Employees.emp_no == Salaries.emp_no).all()
+													
+tList = []
 
-for emp in results: 
-	for t in titles: 
-		print(t.title)
+for row in results: 
+	tDict = {"title": row[1].title, 
+		   "salary": row[2].salary
+	}
+	tList.append(tDict)
+
+title_pd = pd.DataFrame(tList)
+grouped_df = title_pd.groupby(["title"]).mean()
+
+grouped_df.plot(kind = "bar", rot=45)
+plt.title("Average Salary By Department")
+plt.xlabel("Title")
+plt.ylabel("Salary")
+plt.tight_layout()
+
+plt.savefig("salaryBar.png")
