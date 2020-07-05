1838c5cc8229b8cadf64a4f59c3572dde5c7c12e
Mark Pasricha <markp862@gmail.com>
2020-07-05 13:56:23 -0400

    Added Second Graph

--- sqlData.py
+++ sqlData.py
@@ -10,8 +10,9 @@ from tqdm import tqdm
 
 
 engine = create_engine(f'postgresql+psycopg2://{database}:{password}@localhost:5432/company_db', echo=False)
-
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+s = Session()
 
 class Titles(Base): 
 	__tablename__ = 'titles'
@@ -33,36 +34,38 @@ class Employees(Base):
 class Salaries(Base):
 	__tablename__  = 'salaries'
 
-	emp_no = Column(Integer, primary_key=True)
+	emp_no = Column(Integer, primary_key=True )
 	salary = Column(Integer)
 
+def plotBarChart(title_df):
+	grouped_df = title_df.groupby(["title"]).mean()
+	grouped_df.plot(kind = "bar", rot=90)
+	plt.title("Average Salary By Department")
+	plt.xlabel("Title")
+	plt.ylabel("Salary")
+	plt.tight_layout()
+	plt.savefig("salaryBar.png")
 
+def plotHisto(title_df):
+	title_df.hist()
+	plt.xlabel("Salary")
+	plt.ylabel('Count of Employees')
+	plt.title('Most Common Salary Range')
+	plt.savefig("salaryHist.png")
 
+def createDF(session):
+	tList = []
+	results = session.query(Employees, Titles, Salaries).join(Titles,Titles.title_id == Employees.emp_title_id).join(Salaries,Employees.emp_no == Salaries.emp_no).all()
+	for row in results: 
+		tDict = {"title": row[1].title, 
+		   		 "salary": row[2].salary
+				}
+		tList.append(tDict)												
+	df = pd.DataFrame(tList)
 
-Session = sessionmaker(bind=engine)
-s = Session()
+	return df
 
-# result = session.query(Employees).all()
-# results2 = session.query(Salaries).all() 
-# results3 = session.query(Titles).all()
-
-results = s.query(Employees, Titles, Salaries).join(Titles,Titles.title_id == Employees.emp_title_id).join(Salaries,Employees.emp_no == Salaries.emp_no).all()
-													
-tList = []
-
-for row in results: 
-	tDict = {"title": row[1].title, 
-		   "salary": row[2].salary
-	}
-	tList.append(tDict)
-
-title_pd = pd.DataFrame(tList)
-grouped_df = title_pd.groupby(["title"]).mean()
-
-grouped_df.plot(kind = "bar", rot=45)
-plt.title("Average Salary By Department")
-plt.xlabel("Title")
-plt.ylabel("Salary")
-plt.tight_layout()
-
-plt.savefig("salaryBar.png")
+title_df = createDF(s)
+print(title_df)
+plotBarChart(title_df)
+plotHisto(title_df)
